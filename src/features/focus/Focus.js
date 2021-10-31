b399ec5d7244f2b62f7d1ed229632120a034fdd2
Suraj Vijay <surajcpp115@gmail.com>
2021-10-31 14:35:19 +0530

    Initial Entry

---
+++ src/features/focus/Focus.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import { RoundedButton } from '../../components/RoundedButton';
+import { spacing, fontSizes } from "../../utils/sizes";
+import { colors } from "../../utils/colors";
+
+
+export const Focus = ({ addSubject }) => {
+  const [subject, setSubject] = useState(null);
+  
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text style={styles.title}>What would you like to focus on ?</Text>
+        <View style={styles.inputContainer}>
+          <TextInput 
+            style={styles.inputStyle} 
+            onSubmitEditing={({nativeEvent}) => setSubject(nativeEvent.text)}
+          />
+          <RoundedButton title="+" size={50} onPress={() => addSubject(subject)} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: spacing.md,
+    justifyContent: 'center',
+  },
+  title: {
+    color: colors.white,
+    fontWeight: 'bold',
+    fontSize: fontSizes.lg,
+  },
+  inputContainer: {
+    paddingTop: spacing.md,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  inputStyle: {
+    flex: 1,
+    marginRight: spacing.md,
+  },
+});
